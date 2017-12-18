17b138643686cacaf255c09e03f3747ce8ca17b9
Alexey Gorshkov <pexuone@gmail.com>
2017-12-18 23:10:25 +0300

    tags package improvements

--- tags/Tags.go
+++ tags/Tags.go
@@ -2,12 +2,15 @@ package tags
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 )
 
-type Tags []string
+type Tags struct {
+	values []string
+}
 
-func JoinTag(group, name string) string {
+func JoinValue(group, name string) string {
 	group_string := ""
 	if group != "" {
 		group_string = fmt.Sprintf("%s:", group)
@@ -15,54 +18,93 @@ func JoinTag(group, name string) string {
 	return fmt.Sprintf("%s%s", group_string, name)
 }
 
-func SplitTag(data string) (group, name string) {
+func SplitValue(value string) (group, name string) {
 	group = ""
-	name = data
-	if strings.Contains(data, ":") {
-		i_s := strings.SplitN(data, ":", 2)
+	name = value
+	if strings.Contains(value, ":") {
+		i_s := strings.SplitN(value, ":", 2)
 		group = i_s[0]
 		name = i_s[1]
 	}
 	return
 }
 
-func New(values []string) Tags {
-	ret := make([]string, len(values))
-	copy(ret, values)
+func New(values []string) *Tags {
+	ret := new(Tags)
+	ret.values = make([]string, len(values))
+	copy(ret.values, values)
 	return ret
 }
 
-func (self Tags) HaveTag(group, name string) bool {
-
-	string_to_check := JoinTag(group, name)
-
-	for _, i := range self {
-		if i == string_to_check {
+func (self *Tags) HaveValue(value string) bool {
+	for _, i := range self.values {
+		if i == value {
 			return true
 		}
 	}
 	return false
 }
 
-func (self Tags) GetGroup(group string, separate bool) []string {
-	ret := make([]string, 0)
-	for _, i := range self {
-		g, n := SplitTag(i)
-		if g == group {
-			if separate {
-				ret = append(ret, n)
-			} else {
-				ret = append(ret, i)
-			}
+func (self *Tags) AddValue(value string) {
+	if self.HaveValue(value) {
+		return
+	}
+	self.values = append(self.values, value)
+	return
+}
+
+func (self *Tags) DeleteValue(value string) {
+	for i := len(self.values) - 1; i != -1; i-- {
+		if self.values[i] == value {
+			self.values = append(self.values[:i], self.values[i+1:]...)
 		}
 	}
+}
+
+func (self *Tags) Have(group, name string) bool {
+	return self.HaveValue(JoinValue(group, name))
+}
+
+func (self *Tags) HaveGroup(group string) bool {
+	for _, value := range self.values {
+		g, _ := SplitValue(value)
+		if g == group {
+			return true
+		}
+	}
+	return false
+}
+
+func (self *Tags) Add(group, name string) {
+	self.AddValue(JoinValue(group, name))
+}
+
+func (self *Tags) Delete(group, value string) {
+	j := JoinValue(group, value)
+	self.DeleteValue(j)
+}
+
+func (self *Tags) DeleteGroup(group string) {
+	for i := len(self.values) - 1; i != -1; i-- {
+		g, _ := SplitValue(self.values[i])
+		if g == group {
+			self.values = append(self.values[:i], self.values[i+1:]...)
+		}
+	}
+	return
+}
+
+func (self *Tags) Values() []string {
+	ret := make([]string, len(self.values))
+	copy(ret, self.values)
+	sort.Strings(ret)
 	return ret
 }
 
-func (self Tags) Map() map[string]([]string) {
+func (self *Tags) Map() map[string]([]string) {
 	ret := make(map[string]([]string))
-	for _, i := range self {
-		g, n := SplitTag(i)
+	for _, i := range self.values {
+		g, n := SplitValue(i)
 
 		if _, ok := ret[g]; !ok {
 			ret[g] = make([]string, 0)
@@ -74,31 +116,45 @@ func (self Tags) Map() map[string]([]string) {
 	return ret
 }
 
-func (self Tags) Add(
-	group, name string,
-	abort_if_exists bool,
-	remove_existing bool,
-) {
-
-	if self.HaveTag(group, name) {
-		if abort_if_exists {
-			return
-		}
-
-		if remove_existing {
-			self.Delete(group, name)
+func (self *Tags) Get(group string, separate bool) []string {
+	ret := make([]string, 0)
+	for _, i := range self.values {
+		g, n := SplitValue(i)
+		if g == group {
+			if separate {
+				ret = append(ret, n)
+			} else {
+				ret = append(ret, i)
+			}
 		}
 	}
-
-	self = append(self, JoinTag(group, name))
-
+	return ret
 }
 
-func (self Tags) Delete(group, name string) {
-	j := JoinTag(group, name)
-	for i := len(self) - 1; i != -1; i-- {
-		if self[i] == j {
-			self = append(self[:i], self[i+1:]...)
+func (self *Tags) GetSingle(group string, separate bool) (string, bool) {
+	for _, i := range self.values {
+		g, n := SplitValue(i)
+		if g == group {
+			if separate {
+				return n, true
+			} else {
+				return i, true
+			}
 		}
 	}
+	return "", false
+}
+
+func (self *Tags) SetSingle(group, name string) {
+	self.DeleteGroup(group)
+	self.Add(group, name)
+	return
+}
+
+// NOTE: adding value which not contains group, will first delete all other
+//       values which have no group
+func (self *Tags) SetSingleValue(value string) {
+	g, n := SplitValue(value)
+	self.SetSingle(g, n)
+	return
 }
--- tags/test/t01.go
+++ tags/test/t01.go
@@ -6,12 +6,30 @@ import (
 	"github.com/AnimusPEXUS/utils/tags"
 )
 
-func main() {
-	t := tags.New([]string{"parent_project:gnome", "system", "fundamental"})
-	for k, v := range t.Map() {
+func Print(tag *tags.Tags) {
+
+	for k, v := range tag.Map() {
 		fmt.Printf("  '%s'\n", k)
 		for _, i := range v {
 			fmt.Printf("    '%s'\n", i)
 		}
 	}
+	fmt.Println("------------------------")
+}
+
+func main() {
+	t := tags.New(
+		[]string{
+			"parent_project:gnome",
+			"system",
+			"fundamental",
+		},
+	)
+	Print(t)
+
+	t.Add("what?", "this")
+	Print(t)
+
+	t.DeleteGroup("")
+	Print(t)
 }
