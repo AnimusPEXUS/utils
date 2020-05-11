6d1a3a002c751beb7883a8132b3183c0f84453f5
Animus PEXUS <animuspexus@protonmail.com>
2020-05-11 22:58:01 +0300

    some more documentation to MutexCheckable

--- sync/MutexCheckable.go
+++ sync/MutexCheckable.go
@@ -6,14 +6,17 @@ import (
 
 var _ LockerCheckable = &MutexCheckable{}
 
-// difference to sync.Mutex, is what Unlock can be called if already unlocked
-// this can be used in sync.Cond without afraiding to get error when Unlock() called
+// Difference to sync.Mutex, is what Unlock can be called if already unlocked.
+// This can be used in sync.Cond without afraiding to get error when Unlock()
+// called.
+// NewMutexCheckable() must be used to create object of this struct.
 type MutexCheckable struct {
 	mutex_o   *sync_o.Mutex
 	is_locked bool
 	s         *sync_o.Mutex
 }
 
+// set locked to true, to make resulting Mutex already locked on creation
 func NewMutexCheckable(locked bool) *MutexCheckable {
 	self := &MutexCheckable{
 		is_locked: locked,
@@ -26,6 +29,7 @@ func NewMutexCheckable(locked bool) *MutexCheckable {
 	return self
 }
 
+// same as sync.Lock()
 func (self *MutexCheckable) Lock() {
 
 	// to call or not to call self.s.Unlock() in defer
@@ -48,6 +52,8 @@ func (self *MutexCheckable) Lock() {
 	self.is_locked = true
 }
 
+// same as sync.Unlock(), except, doesn't results in error when called on
+// unlocked MutexCheckable
 func (self *MutexCheckable) Unlock() {
 
 	self.s.Lock()
@@ -62,6 +68,7 @@ func (self *MutexCheckable) Unlock() {
 	self.is_locked = false
 }
 
+// returns lock state
 func (self *MutexCheckable) IsLocked() bool {
 	self.s.Lock()
 	defer self.s.Unlock()
